502ff66effcf2245bfcb5dfa98997d2424a8096c
Jason McLeod <pixelparty@gmail.com>
2013-03-07 13:27:31 -0500

    SpriteWithContainer class added, needs some more abstraction. moverate reduced

--- public/js/mainloop.js
+++ public/js/mainloop.js
@@ -1,10 +1,10 @@
 function mainloop($scope) {
 
-    if(key.isPressed("W") || key.isPressed(38)) $scope.me.moveBy({y:-4})
-    if(key.isPressed("S") || key.isPressed(40)) $scope.me.moveBy({y:4})
+    if(key.isPressed("W") || key.isPressed(38)) $scope.me.moveBy({y:-2})
+    if(key.isPressed("S") || key.isPressed(40)) $scope.me.moveBy({y: 2})
 
-    if(key.isPressed("A") || key.isPressed(37)) $scope.me.moveBy({x:-4})
-    if(key.isPressed("D") || key.isPressed(49)) $scope.me.moveBy({x:4})
+    if(key.isPressed("A") || key.isPressed(37)) $scope.me.moveBy({x:-2})
+    if(key.isPressed("D") || key.isPressed(49)) $scope.me.moveBy({x: 2})
 }
 
 $(function() {
--- public/js/models/LocalPlayer.js
+++ public/js/models/LocalPlayer.js
@@ -10,27 +10,10 @@ LocalPlayer.extend({
     join:function() {
         this.socket.emit('join','test')
 
-        // create a container so we can rotate this sprite on it's center
-        var container = new createjs.Container();
-
-        // move to the local middle of the screen
-        container.globalx = this.$scope.camera.width * this.$scope.tileSize / 2;
-        container.globaly = this.$scope.camera.height * this.$scope.tileSize / 2;
-
-        // white box placeholder
-        var graphic = new createjs.Shape();
-        graphic.x = -8;
-        graphic.y = -8;
-        graphic.graphics.beginFill('#FFFFFF').drawRect(0,0,16,16)
-
-        // attach sprite to container
-        container.addChild(graphic);
-
-        // add it to the stage
-        this.$scope.renderer.stage.add(container);
-
-        // make the container available to player as "sprite"
-        this.sprite = container
+        this.sprite = new SpriteWithContainer('/assets/sprites/fed.png', 32, 32)
+        this.sprite.globalx = this.$scope.camera.width *  this.$scope.tileSize / 2;
+        this.sprite.globaly = this.$scope.camera.height * this.$scope.tileSize / 2;
+        this.$scope.renderer.stage.add(this.sprite);
 
         // socket.emit('hi', {yay:'!'})
     }
