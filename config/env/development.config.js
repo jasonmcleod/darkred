525f1141b8f029820eae04a425f3d5481e174580
Jason McLeod <pixelparty@gmail.com>
2013-03-15 00:26:03 -0400

    my local config

--- config/env/development.config.js
+++ config/env/development.config.js
@@ -2,9 +2,9 @@ module.exports = {
     port: 3000,
     database: {
         host:'127.0.0.1',
-        port:3306,//8889,
+        port:8889,
         user:'root',
-        pass:'',
+        pass:'root',
         database:'arpg'
     }
 };
